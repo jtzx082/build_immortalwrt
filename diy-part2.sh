24737f0dc73aa2234ddf0995cdbc47c13a8b0da0
WenJun <nczywq@qq.com>
2024-11-21 17:28:37 +0800

    去除turboacc

--- diy-part2.sh
+++ diy-part2.sh
@@ -34,7 +34,7 @@ function git_sparse_clone() {
 # 添加测速插件
 # git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 # 添加 万能推送
-git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+# git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 # 添加关机插件
 #git clone https://github.com/VPN-V2Ray/luci-app-poweroff.git package/luci-app-poweroff
 # 添加passwall2
@@ -42,9 +42,9 @@ git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 # 添加应用过滤
 # git clone  https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #加入turboacc
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh
-chmod -R 777 add_turboacc.sh
-bash add_turboacc.sh --no-sfe
+#curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh
+#chmod -R 777 add_turboacc.sh
+#bash add_turboacc.sh --no-sfe
 
 
 echo "
@@ -63,7 +63,7 @@ CONFIG_PACKAGE_luci-theme-openwrt-2020=y
 #CONFIG_PACKAGE_luci-app-netspeedtest=y
 
 # 万能推送
-CONFIG_PACKAGE_luci-app-pushbot=y
+#CONFIG_PACKAGE_luci-app-pushbot=y
 
 # 关机插件
 #CONFIG_PACKAGE_luci-app-poweroff=y
@@ -72,7 +72,7 @@ CONFIG_PACKAGE_luci-app-pushbot=y
 #CONFIG_PACKAGE_luci-app-passwall2=y
 
 # TurboAcc
-CONFIG_PACKAGE_luci-app-turboacc=y
+#CONFIG_PACKAGE_luci-app-turboacc=y
 
 " >> .config
 
