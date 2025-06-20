25a5773d32b0706d5cc40747748d33dd27874179
jtzx082 <yujq804@gmail.com>
2025-06-20 22:53:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,7 +32,7 @@ function git_sparse_clone() {
 # git clone https://github.com/derisamedia/luci-theme-alpha.git package/luci-theme-alpha
 
 # 添加测速插件
-# git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
 # 添加 万能推送
 # git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 # 添加关机插件
@@ -42,34 +42,38 @@ function git_sparse_clone() {
 # 添加应用过滤
 # git clone  https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #加入turboacc
-#curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh
-#chmod -R 777 add_turboacc.sh
-#bash add_turboacc.sh --no-sfe
-
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh
+chmod -R 777 add_turboacc.sh
+bash add_turboacc.sh --no-sfe
+#分区扩容
+git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 
 echo "
 # 主题
 #CONFIG_PACKAGE_luci-theme-design=y
 
-CONFIG_PACKAGE_luci-theme-argon=y
+#CONFIG_PACKAGE_luci-theme-argon=y
 
-CONFIG_PACKAGE_luci-theme-material=y
+#CONFIG_PACKAGE_luci-theme-material=y
 
-CONFIG_PACKAGE_luci-theme-openwrt-2020=y
+#CONFIG_PACKAGE_luci-theme-openwrt-2020=y
 
 #CONFIG_PACKAGE_luci-theme-alpha=y
 
 # 测速插件
-#CONFIG_PACKAGE_luci-app-netspeedtest=y
+CONFIG_PACKAGE_luci-app-netspeedtest=y
 
 # 万能推送
 #CONFIG_PACKAGE_luci-app-pushbot=y
 
 # TurboAcc
-#CONFIG_PACKAGE_luci-app-turboacc=y
+CONFIG_PACKAGE_luci-app-turboacc=y
 
 # 应用过滤
 # CONFIG_PACKAGE_luci-app-oaf=y
+
+# 分区扩容
+CONFIG_PACKAGE_luci-app-partexp=y
 
 " >> .config
 
@@ -83,14 +87,14 @@ sed -i 's/255.255.255.0/255.255.252.0/g' package/base-files/files/bin/config_gen
 #sed -i 's/luci-theme-openwrt-2020/luci-theme-alpha/g' feeds/luci/collections/luci/Makefile
 
 # 修改主机名
-sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+#sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
 
 # 修改Ping 默认网址 immortalwrt.org
 #cat feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_network/diagnostics.htm
 
 # 修改系统信息
-cp -f $GITHUB_WORKSPACE/99-default-settings package/emortal/default-settings/files/99-default-settings
-cp -f $GITHUB_WORKSPACE/banner package/base-files/files/etc/banner
+#cp -f $GITHUB_WORKSPACE/99-default-settings package/emortal/default-settings/files/99-default-settings
+#cp -f $GITHUB_WORKSPACE/banner package/base-files/files/etc/banner
 
 # 修改主题背景
 #cp -f $GITHUB_WORKSPACE/argon/img/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
